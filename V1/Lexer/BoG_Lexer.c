c53b94904c7283409f800ffd9dd06c282d33d2e3
GardarAndri <GardarSig95@gmail.com>
2017-02-12 17:26:50 +0000

    Made lexer interface

--- V1/Lexer/BoG_Lexer.c
+++ V1/Lexer/BoG_Lexer.c
@@ -1,13 +1,52 @@
 
 #include "BoG_Lexer_Generated.c"
 #include "stdio.h"
+#include "string.h"
+
+int current_token;
+
+void start_lexer(){
+	current_token = yylex();
+}
+
+int current_token(){
+	return current_token;
+}
+
+int advance(){
+	current_token = yylex();
+
+	if(current_token == BOG_ERROR){
+		printf("An lexing error ocurred!\n");
+		finish_lexing();
+		return BOG_ERROR;
+	}
+
+	if(current_token == 0) return finish_lexing();
+
+	return current_token;
+}
+
+int advance_over(char* targ){
+	current_token = yylex();
+
+	if(strcmp(yytext,targ)){
+		// yytext != targ
+		printf("Expected '%s' but found '%s'\n",targ,yytext);
+		finish_lexing();
+		return BOG_ERROR;
+	}
+
+	if(current_token == BOG_ERROR){
+		printf("An lexing error ocurred!\n");
+		finish_lexing();
+		return BOG_ERROR;
+	}
+
+	if(current_token == 0) return finish_lexing();
+
+	return current_token;
+}
 
 int main(){
-	int current_token = yylex();
-	while(1){
-		printf("%d '%s'\n",current_token,yytext);
-
-		current_token = yylex();
-		if(current_token == 0) break;
-	}
 }
