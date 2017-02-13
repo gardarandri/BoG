0052072babfc340b78e8e57fc4ff156a3a0e4c81
GardarAndri <GardarSig95@gmail.com>
2017-02-13 22:30:15 +0000

    Started changing to nanoMorpho

--- V1/Parser/BoG_Parser.c
+++ V1/Parser/BoG_Parser.c
@@ -3,6 +3,110 @@
 #define BOG_MATCH 5
 #define BOG_NO_MATCH 6
 
+//program 	= function , program
+//			| function
+void program(){
+	while(current_token != BOG_EOF){
+		function();
+	}
+
+	return;
+}
+
+//function	= NAME , "(" , args , ")" , "{" , { decl , ";" } , { expr , ";" } , "}"
+void function(){
+	advance_over_BOG(BOG_NAME_ENUM, "name");
+
+	advance_over_str("(");
+
+	args();
+
+	advance_over_str(")");
+	advance_over_str("{");
+
+	while(current_token == BOG_VAR){
+		decl();
+	}
+
+	while(!lexeme_equals("}")){
+		expr();
+	}
+
+	advance_over_str("}");
+}
+
+//decl		= "var" , NAME , { NAME }
+void decl(){
+	advance_over_str("var");
+
+	advance_over_BOG(BOG_NAME_ENUM);
+	while(current_token == BOG_NAME_ENUM){
+		advance_over_BOG(BOG_NAME_ENUM);
+	}
+}
+
+//expr		= NAME
+//			| NAME , "=" , expr
+//			| NAME , "(" , [ expr , { "," , expr } ] , ")"
+//			| "return" , expr
+//			| OPNAME , expr
+//			| expr OPNAME expr
+//			| LITERAL
+//			| "(" , expr , ")"
+//			| ifexpr
+//			| "while" , "(" , expr , ")" , body
+void expr(){
+	if(current_token == BOG_NAME_ENUM){
+	}else if(current_token == BOG_RETURN){
+	}else if(current_token == BOG_OPNAME){
+	}else if(current_token == BOG_LITERAL){
+	}else if(lexeme_equals("(")){
+	}else if(current_toke == BOG_IF){
+	}else if(current_token == BOG_WHILE){
+	}else{
+		throw_error("Expression");
+	}
+
+	if(current_token == BOG_OPNAME){
+		advance();
+		expr();
+	}
+}
+
+//ifexpr		= "if" , "(" , expr , ")" , body , [ { "elsif" , "(" , expr , ")" , body } ] , [ "else" , body ]
+void ifexpr(){
+	advance_over_str("if");
+	advance_over_str("(");
+
+	expr();
+
+	advance_over_str(")");
+
+	body();
+
+	// TODO: Add elsif as a token in the lexer
+	while(current_token == BOG_ELSIF){
+	}
+}
+
+//body		= "{" , expr , ";" , { expr , ";" } , "}"
+void body(){
+	advance_over_str("{");
+
+	expr();
+	advance_over_str(";");
+
+	while(!lexeme_equals("}")){
+		expr();
+	}
+
+	advance();
+}
+
+
+
+
+
 //TODO: Beytta í nanomorpho
 
 //program 	= fundcel , ";" , program
@@ -74,112 +178,4 @@ int vdt2(){
 	if(expr() == BOG_NO_MATCH) return BOG_NO_MATCH;
 
 	if(vdt3() == BOG_NO_MATCH) return BOG_NO_MATCH;
-}
-
-//vdt3		= ""
-//			| "," , vdt1
-int vdt3(){
-}
-
-//expr		= "return" , expr
-//			| NAME , "=" , expr
-//			| orexpr
-int expr(){
-}
-
-//orexpr		= andexpr , orexprt
-int orexpr(){
-}
-
-//orexpr1		= "||" , orexpr
-int orexpr1(){
-}
-//			| ""
-
-//andexpr		= notexpr , andexprt
-int andexpr(){
-}
-
-//andexprt	= "&&" , andexpr
-//			| ""
-int andexprt(){
-}
-
-//notexpr		= "!" , notexpr
-//			| opexpr
-int notexpr(){
-}
-
-//opexpr		= smallexpr , opexprt
-int opexpr(){
-}
-
-//opexprt		= OPNAME , opexpr
-//			| ""
-int opexprt(){
-}
-
-//smallexpr	= NAME , smallexprn
-//			| OPNAME , expr
-//			| LITERAL
-//			| "(" , expr , ")"
-//			| ifexpr
-//			| "while" , "(" , expr , ")" , body
-//			| fundecl
-int smallexpr(){
-}
-
-//smallexprn	= ""
-//			| "(" , manyexpr , ")"
-int smallexprn(){
-}
-
-//manyexpr	= expr , manyexprt
-int manyexpr(){
-}
-
-//manyexprt	= "," , manyexpr
-//			| ""
-int manyexprt(){
-}
-
-//ifexpr		= "if" , "(" , expr , ")" , body , ifexprt1
-int ifexpr(){
-}
-
-//ifexprt1	= ""
-//			| "elseif" , "(" , expr , ")" , body , ifexprt2
-int ifexprt1(){
-}
-
-//ifexprt2	= ""
-//			| "else" , body
-int ifexprt2(){
-}
-
-//body		= "{" , innerbody , "}"
-int body(){
-	vardecl();
-	expr();
-}
-
-//innerbody	= funcdecl , ";" , innerbodyt
-//			| vardecl , ";" , innerbodyt
-//			| expr , ";" , innerbodyt
-int innerbody(){
-}
-
-//innerbodyt	= ""
-//			| funcdecl , ";" , innerbodyt
-//			| vardecl , ";" , innerbodyt
-//			| expr , ";" , innerbodyt
-int innerbodyt(){
-}
-
-int main(){
-	try{
-		program
-	}catch(error){
-		printf();
-	}
 }
