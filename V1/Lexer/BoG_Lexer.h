33be18782b6ec89de8542a47ed6b02ba1c46775e
GardarAndri <GardarSig95@gmail.com>
2017-02-13 22:30:31 +0000

    Started changing to nanoMorpho

--- V1/Lexer/BoG_Lexer.h
+++ V1/Lexer/BoG_Lexer.h
@@ -7,6 +7,11 @@
 
 int current_token;
 
+void throw_error(char* expected_token){
+	printf("Expected '%s' but found '%s'\n",expected_token,yytext);
+	exit(0);
+}
+
 void start_lexer(){
 	current_token = yylex();
 }
@@ -32,24 +37,28 @@ int advance(){
 
 	if(current_token == BOG_ERROR){
 		printf("An lexing error ocurred!\n");
-		finish_lexing();
-		return BOG_ERROR;
+		exit(0);
 	}
 
-	if(current_token == 0) return finish_lexing();
+	if(current_token == BOG_EOF) exit(0);
 
 	return current_token;
 }
 
-int advance_over(char* targ){
+int advance_over_BOG(int BOG, char* expected_token_name){
+	if(current_token != bog_enum){
+		// TODO: Geta gert custom errors
+		throw_error(expected_token_name);
+	}
+	advance();
+}
+
+int advance_over_str(char* targ){
 	if(strcmp(yytext,targ)){
 		// yytext != targ
-		printf("Expected '%s' but found '%s'\n",targ,yytext);
-		finish_lexing();
-		return BOG_ERROR;
+		throw_error(targ);
 	}
-
-	return advance();
+	advance();
 }
 
 
