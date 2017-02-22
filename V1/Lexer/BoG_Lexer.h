05f46be4e560a569af8b4352b8af88a716702fab
GardarAndri <GardarSig95@gmail.com>
2017-02-22 10:54:37 +0000

    Lexer now supports lookahead of 2

--- V1/Lexer/BoG_Lexer.h
+++ V1/Lexer/BoG_Lexer.h
@@ -1,10 +1,16 @@
+#include "string.h"
 #include "BoG_Lexer_Generated.c"
 #include "stdio.h"
-#include "string.h"
 
 #define BOG_FINISHED 999
+#define BOG_LEXEME_MAX_LEN 4096
 
+// Breyta sem inniheldur tókið sem lesgreinirinn labbar næst yfir
 int current_token;
+// Breyta sem inniheldur tókið sem lesgreinirinn labbar þar næst yfir
+int next_token;
+
+char current_lexeme[BOG_LEXEME_MAX_LEN];
 
 // Notkun:	throw_error(s);
 // Fyrir:	s er strengur
@@ -14,8 +20,26 @@ int current_token;
 //			c er stafanúmerið, s er það sem búist var við
 //			en r er það sem lesgreinirinn fann.
 void throw_error(char* expected_token){
-	printf("line %d, char %d: Expected '%s' but found '%s'\n",lexer_at_line,lexer_at_char,expected_token,yytext);
+	printf("line %d, char %d: Expected '%s' but found '%s'\n",lexer_at_line,lexer_at_char,expected_token,current_lexeme);
 	exit(0);
+}
+
+// Notkun:	copy_lexeme_over()
+// Fyrir:	Lesgreining er í gangi
+// Eftir:	Búið er að afrita yytext í breytuna
+// 			current_lexeme eða gefa villu ef yytext er of
+// 			langur strengur.
+void copy_lexeme_over(){
+	int i=0;
+	while(1){
+		if(i >= BOG_LEXEME_MAX_LEN){
+			printf("line %d, char %d: Lexeme to large! (This might be caused by a literal being to long)",lexer_at_line,lexer_at_char);
+			exit(1);
+		}
+		current_lexeme[i] = yytext[i];
+		if(yytext[i] == '\0') break;
+		i++;
+	}
 }
 
 // Notkun:	start_lexer(s);
@@ -23,7 +47,13 @@ void throw_error(char* expected_token){
 // Eftir:	Lesgreining er hafin á skránni s
 void start_lexer(const char* input_file){
 	yyin = fopen(input_file, "r");
+	if(!yyin){
+		printf("%s: Fatal: No such file!\n", input_file);
+		exit(1);
+	}
 	current_token = yylex();
+	copy_lexeme_over();
+	next_token = yylex();
 }
 
 // Notkun:	finish_lexing();
@@ -36,7 +66,7 @@ int finish_lexing(){
 
 /*
 int current_lexeme_size(){
-	return sizeof(char) * strlen(yytext);
+	return sizeof(char) * (strlen(yytext)+1);
 }
 */
 
@@ -46,12 +76,13 @@ void current_lexeme(char* write_to){
 }
 */
 
+
 // Notkun:	b = lexeme_equals(s)
 // Fyrir:	s er strengur
 // Eftir:	b er satt þ.þ.a.a. tókið sem lesgreinirinn
 // 			er á hefur sömu strengjaframsetningu og s
 int lexeme_equals(char* query){
-	return !strcmp(yytext,query);
+	return !strcmp(current_lexeme,query);
 }
 
 // Notkun:	advance(b);
@@ -70,7 +101,9 @@ int advance(
 	printf("\n");
 #endif
 
-	current_token = yylex();
+	current_token = next_token;
+	copy_lexeme_over();
+	next_token = yylex();
 
 	if(current_token == BOG_ERROR){
 		printf("line %d: An lexing error ocurred! Could not lex '%s'.\n",lexer_at_line,yytext);
@@ -93,7 +126,7 @@ int advance_over_BOG(
 	if(current_token != bog_enum){
 		throw_error(enum_to_str(bog_enum));
 	}
-	advance(
+	return advance(
 #ifdef LEXER_DEBUG
 			parser_line
 #endif
@@ -111,11 +144,11 @@ int advance_over_str(
 		int parser_line,
 #endif
 		char* targ){
-	if(strcmp(yytext,targ)){
+	if(strcmp(current_lexeme,targ)){
 		// yytext != targ
 		throw_error(targ);
 	}
-	advance(
+	return advance(
 #ifdef LEXER_DEBUG
 			parser_line
 #endif
